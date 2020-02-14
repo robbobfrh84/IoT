03d9553286f770b92d21cb6514093e371798e156
Bob Main <bobmain49@gmail.com>
2020-02-13 20:45:28 -0600

    lasypush

--- DemoReel100/DemoReel100.ino
+++ DemoReel100/DemoReel100.ino
@@ -15,11 +15,11 @@ FASTLED_USING_NAMESPACE
 #warning "Requires FastLED 3.1 or later; check github for latest code."
 #endif
 
-#define DATA_PIN    24
+#define DATA_PIN    12
 //#define CLK_PIN   4
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
-#define NUM_LEDS    12
+#define NUM_LEDS    24
 CRGB leds[NUM_LEDS];
 
 #define BRIGHTNESS          96
