f262045ce4e70d1cb9316f3dbb4d6913a5e99894
robbobfrh84 <bobmain49@gmail.com>
2020-02-22 11:27:37 -0600

    adding proof of concept for 32 hall sensors to addressed led strip

--- hallLED32/hallLED32.ino
+++ hallLED32/hallLED32.ino
@@ -1,39 +1,84 @@
+#include "ShiftIn.h"
 #include <FastLED.h>
-#define NUM_LEDS 24
+#define NUM_LEDS 32
+#define CHIPS 4
+
+const int ledStripPin = 7;
+
+ShiftIn<CHIPS> shift;
 
 CRGB ledStrip[NUM_LEDS];
 
-const int ledStripPin = 7;
-const int selectPins[3] = {2, 3, 4}; 
-const int readPin = 5; 
-
 void setup() {
+  Serial.begin(9600); // declare pins: pLoadPin, clockEnablePin, dataPin, clockPin
+  shift.begin(8, 9, 11, 12);
   FastLED.addLeds<NEOPIXEL,ledStripPin>(ledStrip, NUM_LEDS); 
-  for (int i=0; i<3; i++) {
-    pinMode(selectPins[i], OUTPUT);
-    digitalWrite(selectPins[i], HIGH);
+}
+
+void displayValues() {
+  int chipCnt = CHIPS * 8;
+  int bitCnt = 0;
+  for(int i = 0; i < shift.getDataWidth(); i++) { // print out all 8 buttons
+    bitCnt++;
+    if (i%8 == 0) {
+      chipCnt -= 8;
+      bitCnt = 0;
+    }
+    Serial.print( String(!shift.state(i)) ); // get state of button i
+    if (!shift.state(i)) {
+      ledStrip[bitCnt + chipCnt] = CRGB(40,0,40);
+    } else {
+      ledStrip[bitCnt + chipCnt] = CRGB(0,0,0);
+    }
   }
-  pinMode(readPin, INPUT);
-  Serial.begin(9600);
+  FastLED.show();
+  Serial.println();
 }
 
 void loop() {
-  FastLED.show();
-  for (byte pin=0; pin<=7; pin++) {
-    for (int i=0; i<3; i++) {
-      digitalWrite(selectPins[i], pin & (1<<i)?HIGH:LOW);
-    }
-    int connects = digitalRead(readPin);
-    if (!connects) {
-      ledStrip[pin] = CRGB(40,0,40);
-      ledStrip[pin+12] = CRGB(40,0,40);
-    } else {
-      ledStrip[pin] = CRGB(0,0,0);
-      ledStrip[pin+12] = CRGB(0,0,0);
-    }
-    // Serial.print(String(!connects));
+  if(shift.update()) { // read in all values. returns true if any button has changed
+    displayValues();
   }
-  //  Serial.println();
   delay(1);
-
 }
+
+
+// #include <FastLED.h>
+// #define NUM_LEDS 24
+
+// CRGB ledStrip[NUM_LEDS];
+
+// const int ledStripPin = 7;
+// const int selectPins[3] = {2, 3, 4}; 
+// const int readPin = 5; 
+
+// void setup() {
+//   FastLED.addLeds<NEOPIXEL,ledStripPin>(ledStrip, NUM_LEDS); 
+//   for (int i=0; i<3; i++) {
+//     pinMode(selectPins[i], OUTPUT);
+//     digitalWrite(selectPins[i], HIGH);
+//   }
+//   pinMode(readPin, INPUT);
+//   Serial.begin(9600);
+// }
+
+// void loop() {
+//   FastLED.show();
+//   for (byte pin=0; pin<=7; pin++) {
+//     for (int i=0; i<3; i++) {
+//       digitalWrite(selectPins[i], pin & (1<<i)?HIGH:LOW);
+//     }
+//     int connects = digitalRead(readPin);
+//     if (!connects) {
+//       ledStrip[pin] = CRGB(40,0,40);
+//       ledStrip[pin+12] = CRGB(40,0,40);
+//     } else {
+//       ledStrip[pin] = CRGB(0,0,0);
+//       ledStrip[pin+12] = CRGB(0,0,0);
+//     }
+//     // Serial.print(String(!connects));
+//   }
+//   //  Serial.println();
+//   delay(1);
+
+// }
