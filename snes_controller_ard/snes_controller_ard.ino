353cd43f48c094094312dbfd3be8c5f1232e7266
robbobfrh84 <bobmain49@gmail.com>
2021-04-13 14:13:45 -0500

    lazypush

---
+++ snes_controller_ard/snes_controller_ard.ino
@@ -0,0 +1,13 @@
+int LatchPin  = 8; // Latch
+int ClockPin  = 9; // Clock
+int DataPin   = 10; // Serial Data
+
+unsigned int ControllerData = 0;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  //...              
+}
